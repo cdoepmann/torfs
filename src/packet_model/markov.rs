cdoepmann/torfs

commit d90985fed0ce1aff1c7a11aa2791617f1e893ba6

Make sure we do not overflow time from the Markov chain.

src/packet_model/markov.rs
@@ -72,7 +72,7 @@ impl Display for MarkovChain {
  * 5. update the state
  */
 impl MarkovChain {
-    pub fn get_next(self: &mut Self) -> (DateTime<Utc>, Emission) {
+    pub fn get_next(&mut self, not_after: DateTime<Utc>) -> (DateTime<Utc>, Emission) {
         /* No more hops after the generations has stopped */
         if self.stopped {
             return (self.current_time, Emission::StopGenerating);
@@ -87,7 +87,14 @@ impl MarkovChain {
         if let Emission::StopGenerating = emission {
             self.stopped = true;
         }
-        self.current_time = time + delay;
+
+        // make sure the current time does not leave the valid time range and
+        // does not overflow
+        if delay >= (not_after - self.current_time) {
+            self.current_time = not_after;
+            self.stopped = true;
+        }
+
         self.current_state = next_state.id.clone();
         return (time, emission);
     }

src/packet_model/mod.rs
@@ -29,7 +29,7 @@ impl PacketStream {
         let mut res = Vec::new();
 
         loop {
-            let (time, emission) = self.chain.get_next();
+            let (time, emission) = self.chain.get_next(not_after);
             // println!("{}:{}", time, emission);
 
             if time > not_after {
@@ -83,6 +83,7 @@ impl PacketModelParameters {
 /// A flow that generates new streams
 pub struct FlowOfStreams {
     chain: markov::MarkovChain,
+    not_after: DateTime<Utc>,
 }
 
 impl FlowOfStreams {
@@ -96,7 +97,7 @@ impl Iterator for FlowOfStreams {
     type Item = DateTime<Utc>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let (time, emission) = self.chain.get_next();
+        let (time, emission) = self.chain.get_next(self.not_after);
 
         match emission {
             Emission::GeneratePacketFromClientToServer => {
@@ -131,9 +132,10 @@ impl StreamModelParameters {
         })
     }
 
-    pub fn make_flow(&self, time: DateTime<Utc>) -> FlowOfStreams {
+    pub fn make_flow(&self, time: DateTime<Utc>, not_after: DateTime<Utc>) -> FlowOfStreams {
         FlowOfStreams {
             chain: markov::MarkovChain::new((*self.model).clone(), time),
+            not_after,
         }
     }
 }

src/user.rs
@@ -174,7 +174,10 @@ impl Iterator for PrivcountUser {
                 None => {
                     // there is no active flow, we have to start one
                     let flow_time = self.flow_model.next().unwrap(); // this is an infinite stream, so unwrap is fine
-                    self.current_flow = Some(self.stream_model_parameters.make_flow(flow_time));
+                    self.current_flow = Some(
+                        self.stream_model_parameters
+                            .make_flow(flow_time, self.not_after),
+                    );
                 }
             }
         }

commit 8388f58dca81bd29a13a410116a77fe8e29ebde0

Fix broken Markov chain evaluation.
This bug was introduced in d90985fe.

src/packet_model/markov.rs
@@ -93,6 +93,8 @@ impl MarkovChain {
         if delay >= (not_after - self.current_time) {
             self.current_time = not_after;
             self.stopped = true;
+        } else {
+            self.current_time = time + delay;
         }
 
         self.current_state = next_state.id.clone();